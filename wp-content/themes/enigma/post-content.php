2ed02aef0976b8c6dc698eac352cb6bcd7cdbc26
vmartinyan <vahram.martinyan@hotmail.com>
2016-12-11 11:50:15 +0400

    11-12-2016

--- wp-content/themes/enigma/post-content.php
+++ wp-content/themes/enigma/post-content.php
@@ -1,5 +1,5 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class('enigma_blog_full'); ?>>
-	<ul class="blog-date-left">
+	<!--<ul class="blog-date-left">
 		<li class="enigma_post_date">
 		<?php if ( ('d M  y') == get_option( 'date_format' ) ) : ?>
 		<span class="date"><?php echo get_the_date('d'); ?></span><h6><?php echo get_the_date('M  y'); ?></h6>
@@ -9,7 +9,7 @@
 		</li>
 		<li class="enigma_post_author"><?php echo get_avatar(get_the_author_meta( 'ID' ),96); ?></li>
 		<li class="enigma_blog_comment"><i class="fa fa-comments-o"></i><h6><?php comments_popup_link( '0', '1', '%', '', '-'); ?></h6></li>
-	</ul>
+	</ul>-->
 	<div class="post-content-wrap">
 		<?php if(has_post_thumbnail()): 
 		$img = array('class' => 'enigma_img_responsive'); ?>
@@ -30,7 +30,7 @@
 		<?php endif; ?>
 		<div class="enigma_fuul_blog_detail_padding">
 		<h2><?php if(!is_single()) {?><a href="<?php the_permalink(); ?>"><?php } ?><?php the_title(); ?></a></h2>
-		<div class="row">
+		<!--<div class="row">
 		<div class="col-md-6 col-sm-3">
 		<?php if(get_the_tag_list() != '') { ?>
 		<p class="enigma_tags"><?php the_tags( __('Tags : ','enigma'), '', '<br />'); ?></p>
@@ -42,7 +42,7 @@
 		the_category(' , '); ?></p>
 		<?php } ?>
 		</div>
-		</div>
+		</div>-->
 		<?php the_content( __( 'Read More' , 'enigma' ) ); 
 		$defaults = array(
               'before'           => '<div class="enigma_blog_pagination"><div class="enigma_blog_pagi">' . __( 'Pages:','enigma'  ),
